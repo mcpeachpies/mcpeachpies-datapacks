897a9cf615210ed198988647fa682ec69b2bece8
iGalaxyYT <contact@igalaxy.dev>
2022-12-01 02:00:32 -0800

    feat: gen all possible pronouns

--- pronouns/.generate_teams/src/index.ts
+++ pronouns/.generate_teams/src/index.ts
@@ -1,1 +1,17 @@
 import data from "../data.json" assert { type: "json" };
+
+const pronouns = new Set<string>();
+
+data.pronouns.single.forEach((single) => pronouns.add(single));
+data.pronouns.pairs.forEach((x) => {
+  data.pronouns.pairs.forEach((y) => {
+    if (x != y) pronouns.add(`${x}/${y}`);
+  });
+});
+data.pronouns.triples.forEach((x) => {
+  data.pronouns.triples.forEach((y) => {
+    data.pronouns.triples.forEach((z) => {
+      if (x != y && y != z && x != z) pronouns.add(`${x}/${y}/${z}`);
+    });
+  });
+});
